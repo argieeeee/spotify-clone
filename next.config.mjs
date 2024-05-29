2c0f4949c6671c8b644d8d446b3e4da834b3b5a2
Argie L <argie.lumapac@gmail.com>
2024-05-29 22:54:19 +0800

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["uiuaicifmfdfjewwhaae.supabase.co"],
+	},
+};
 
 export default nextConfig;
