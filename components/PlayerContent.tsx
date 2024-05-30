f142dc57857bd722763e3a1ba3ca83edceb51089
Argie L <argie.lumapac@gmail.com>
2024-05-30 14:28:31 +0800

    added global.d.ts to fix use-sound and added //@ts-ignore to route.ts to fix error

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,10 +1,10 @@
 "use client";
 
+import useSound from "use-sound";
 import {useEffect, useState} from "react";
 import {BsPauseFill, BsPlayFill} from "react-icons/bs";
 import {HiSpeakerWave, HiSpeakerXMark} from "react-icons/hi2";
 import {AiFillStepBackward, AiFillStepForward} from "react-icons/ai";
-import useSound from "use-sound";
 
 import {Song} from "@/types";
 import usePlayer from "@/hooks/usePlayer";
